212630e4974b4d007b6a433da424a1ecf20ac7a9
rlt202 <danik.nikulin@gmail.com>
2020-12-10 12:29:43 +0300

    Initial Commit

---
+++ trafficLigths SwiftUI/trafficLigths_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLigths_SwiftUIApp.swift
+//  trafficLigths SwiftUI
+//
+//  Created by Даниил Никулин on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct trafficLigths_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
